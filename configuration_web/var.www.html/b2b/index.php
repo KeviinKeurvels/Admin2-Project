073aed695b3f4d83784f8cae5024dc6ba0defd8f
Kevin Keurvels <k.keurvels@students.ephec.be>
2022-05-08 12:59:24 +0200

    Installation php changements

---
+++ configuration_web/var.www.html/b2b/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Page b2b</title>
+	</head>
+	<body>
+		<h1>liste dans la db</h1>
+		<?php
+		phpinfo();
+		?>
+	</body>
+</html>
