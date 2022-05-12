608442d05ed0574219b4653df3b44d94ae8aa818
Kevin Keurvels <k.keurvels@students.ephec.be>
2022-05-12 21:45:46 +0200

    Finalisation index.php

--- configuration_web/var.www.html/b2b/index.php
+++ configuration_web/var.www.html/b2b/index.php
@@ -9,9 +9,10 @@
 
 <?php
 try {
+    echo "<table><thead><tr><th>Product number</th><th>Name of product</th><th>Price</th><th>Quantity</th></thead><tbody>";
     $dbh = new PDO('mysql:host=127.0.0.1;dbname=b2bDatabase', "user", "user");
     foreach($dbh->query('SELECT * from PRODUCT') as $row) {
-        echo($row["NAME"]);
+        echo("<tr><td>".$row["NPRO"]."</td><td>".$row["NAME"]."</td><td>".$row["PRICE"]."</td><td>".$row["QUANTITY"]."</td></tr>" );
     }
     $dbh = null;
 } catch (PDOException $e) {
