24deec4083e3bcb725367b7a1c812dbfb66a9e3d
Kevin Keurvels <k.keurvels@students.ephec.be>
2022-05-12 21:25:30 +0200

    Finalisation de la configuration

--- configuration_web/var.www.html/b2b/index.php
+++ configuration_web/var.www.html/b2b/index.php
@@ -1,13 +1,23 @@
 <!doctype html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Page b2b</title>
-	</head>
-	<body>
-		<h1>liste dans la db</h1>
-		<?php
-		phpinfo();
-		?>
-	</body>
+        <head>
+                <meta charset="UTF-8">
+                <title>Page b2b</title>
+        </head>
+        <body>
+                <h1>liste dans la db</h1>
+
+<?php
+try {
+    $dbh = new PDO('mysql:host=127.0.0.1;dbname=b2bDatabase', "user", "user");
+    foreach($dbh->query('SELECT * from PRODUCT') as $row) {
+        echo($row["NAME"]);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
+        </body>
 </html>
